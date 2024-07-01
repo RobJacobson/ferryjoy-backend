1472ed9e39eacf303796824b1c00e17ef835a977
Rob Jacobson <rljacobson@outlook.com>
2024-06-30 18:17:34 -0700

    Prettier

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,17 +1,17 @@
-import { Pool } from 'pg';
-import { migrate } from 'drizzle-orm/node-postgres/migrator';
-import { NodePgDatabase, drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from "pg";
+import { migrate } from "drizzle-orm/node-postgres/migrator";
+import { NodePgDatabase, drizzle } from "drizzle-orm/node-postgres";
 
 async function main() {
   const pool = new Pool({ connectionString: process.env.DATABASE_URL });
   const db: NodePgDatabase = drizzle(pool);
-  
-  console.log('[migrate] Running migrations ...');
-  
-  await migrate(db, { migrationsFolder: './drizzle' });
-  
-  console.log('[migrate] All migrations have been ran, exiting.');
-  
+
+  console.log("[migrate] Running migrations ...");
+
+  await migrate(db, { migrationsFolder: "./src/db/migrations" });
+
+  console.log("[migrate] All migrations have been ran, exiting.");
+
   await pool.end();
 }
 
