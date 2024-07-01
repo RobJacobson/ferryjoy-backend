988ff8650f717a3a3fc369d9545c925df330a23c
Rob Jacobson <rljacobson@outlook.com>
2024-06-30 22:51:32 -0700

    Add route for VesselLocations

---
+++ src/wsf/fetchWsf.ts
@@ -0,0 +1,37 @@
+import axios from "axios";
+// https://blog.logrocket.com/how-to-make-http-requests-like-a-pro-with-axios
+
+const WSF_KEY = "9e61c697-3c2f-490e-af96-72d4e8ecbc7e";
+const DATE_START = 6;
+const DATE_END = 19;
+
+const toUrl = (endpoint: string) =>
+  `https://wsdot.wa.gov/ferries/api/${endpoint}?apiaccesscode=${WSF_KEY}`;
+
+// Axios transformer handler to revive the data using JSON.parse()
+const wsfTransformer = (data: string) => JSON.parse(data, wsfReviver);
+
+// Reviver function for JSON.parse() to import WSDOT datestrings
+const wsfReviver = (key: string, value: any) => {
+  if (typeof value === "string" && value.startsWith("/Date(")) {
+    return toDate(value);
+  }
+  return value;
+};
+
+// Parses a WSDOT timestamp from Unix epoch time into a JS Date
+export const toDate = (wsfDate: string) => {
+  const dateStamp = wsfDate.slice(DATE_START, DATE_END);
+  const secondsSinceEpoch = Number(dateStamp);
+  return new Date(secondsSinceEpoch);
+};
+
+// Fetching function customized for the Wsf API.
+export const fetchWsf = async (endpoint: string) => {
+  const url = toUrl(endpoint);
+  const results = await axios.get(url, {
+    transformResponse: wsfTransformer,
+  });
+  console.log(`Fetched ${JSON.stringify(results.data).length} bytes`);
+  return results.data;
+};
