fa987c03e210fe72b5e40b1ed61612204788b564
Rob Jacobson <rljacobson@outlook.com>
2024-06-30 22:49:53 -0700

    Update index for API

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,10 @@
 import { Elysia } from "elysia";
-import { swagger } from '@elysiajs/swagger'
+import { swagger } from "@elysiajs/swagger";
+import vesselLocationsRoute from "./routes/vesselLocations/index";
 
 const app = new Elysia()
   .use(swagger())
-  .group('/api', (app) =>
-    app.get('/', () => {
-      return "Hello World!"
-    })
-  )
+  .group("/api", (app) => app.use(vesselLocationsRoute))
   .listen(process.env.PORT ?? 3000);
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
